221e8bf823eb09123f2fa8cbb2cedfa08cf8540d
NKLuan2501 <nguyenkhaluan2501@gmail.com>
2026-04-13 22:19:44 +0700

    Cập nhật connectDB

--- src/kqlhotel/dao/ConnectDB.java
+++ src/kqlhotel/dao/ConnectDB.java
@@ -19,7 +19,7 @@ public class ConnectDB {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String url = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyKhachSan;encrypt=false";
             String user = "sa";
-            String password = "sapassword";
+            String password = "123456";
             connection = DriverManager.getConnection(url, user, password);
             System.out.println("Ket noi Database thanh cong!!!!");
         }
